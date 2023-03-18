7da4fc5b588bf839ab6b41790523889b603456e2
Infamous Koala <75808182+infamous-koala@users.noreply.github.com>
2023-03-18 22:36:49 +0530

    Update interpreter.py

--- src/interpreter.py
+++ src/interpreter.py
@@ -7,20 +7,14 @@ def BasicEval(expression):
     x = eval(expression)
     return x
 
-def Integration():
+def Integration(input):
     filename = sys.argv[1]
     file = open(filename,"r")
     input = file.read()
-    i = "i"
-    n = "n"
-    t = "t"
-    brackets = "()"
-    structure = i+n+t+brackets
-    if input == structure:
-        if input == "int()":
-            print("Function is empty!")
-        elif input == "int(dx)":
-            x = "x"
-            return x
+    if input == "int()":
+        print("Function is empty!")
+    elif input == "int(dx)":
+        x = "x"
+        return x
     else:
         print("Syntax error, Invalid syntax in file", filename)
