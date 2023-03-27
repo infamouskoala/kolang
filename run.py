54d95e15337060e2ad44472edaff39623bb9c542
Infamous Koala <75808182+infamous-koala@users.noreply.github.com>
2023-03-27 20:29:20 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from src.ext import Ext
-from src.interpreter import BasicEval, Integration
+from src.interpreter import BasicEval, BasicIntegration
 import sys
 
 filename = sys.argv[1]
@@ -7,7 +7,6 @@ Ext()
 
 file = open(filename,"r")  
 reader = file.read()
-word = reader.split()
 
 # basic eval
 for i in reader:
@@ -16,8 +15,4 @@ for i in reader:
 
 # Integration
     elif i[0] == "I" or i[0] == "i":
-        print(Integration(reader))
-
-# Matrix
-    elif i[0] == "M" or i[0] == "m":
-        print("")
+        print(BasicIntegration(reader))
