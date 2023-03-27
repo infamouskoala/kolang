47bbca79b31f7713641c636273f04fa2d561776e
Infamous Koala <75808182+infamous-koala@users.noreply.github.com>
2023-03-27 20:37:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -9,10 +9,13 @@ file = open(filename,"r")
 reader = file.read()
 
 # basic eval
-for i in reader:
-    if i[0] in ["1","2","3","4","5","6","7","8","9","0"]:
-        print(BasicEval(reader))
+try:
+    for i in reader:
+        if i[0] in ["1","2","3","4","5","6","7","8","9","0"]:
+            print(BasicEval(reader))
 
 # Integration
-    elif i[0] == "I" or i[0] == "i":
-        print(BasicIntegration(reader))
+        elif i[0] == "I" or i[0] == "i":
+            print(BasicIntegration(reader))
+except:
+    print("[!] INVALID SYNTAX")
